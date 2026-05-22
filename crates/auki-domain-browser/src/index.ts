04f8b50087799893c8ad765abaa44f558d8fe8cd
Jonathan Borg <jonathan@aukilabs.com>
2026-05-22 12:45:16 +0800

    delete auki-domain-browser

--- crates/auki-domain-browser/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./contract.js";
-export * from "./discovery.js";
-export * from "./errors.js";
-export * from "./identity.js";
-export * from "./installGlobal.js";
-export * from "./peer.js";
